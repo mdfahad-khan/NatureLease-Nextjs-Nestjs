a73b51232f06b2ef326b16c1852e04190016c168
fahad <evanahmedfahad@gmail.com>
2024-01-28 20:02:45 +0600

    modified

--- land-rental-frontend/pages/Cart/admin.jsx
+++ land-rental-frontend/pages/Cart/admin.jsx
@@ -1,17 +1,17 @@
 
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Admindashboard from "./AdminDashboard"
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
+// import styles from '@/styles/Home.module.css'
+// import Admindashboard from "./AdminDashboard"
 
 
  
-export default function Home() {
-  return (
-    <div>
-        <AdminDashboard />
-     </div>
-  )
-}
+// export default function Home() {
+//   return (
+//     <div>
+//         <AdminDashboard />np
+//      </div>
+//   )
+// }
  
